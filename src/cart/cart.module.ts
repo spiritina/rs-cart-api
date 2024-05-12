10d1099c3fe2b73f37ac182c65765b6a6ba83683
Olga Novak <olga_novak@epam.com>
2024-05-12 19:19:28 +0200

    update

--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -4,11 +4,11 @@ import { OrderModule } from '../order/order.module';
 
 import { CartController } from './cart.controller';
 import { CartService } from './services';
-
+import { DatabaseService } from 'src/database/database.service';
 
 @Module({
-  imports: [ OrderModule ],
-  providers: [ CartService ],
-  controllers: [ CartController ]
+  imports: [OrderModule],
+  providers: [CartService, DatabaseService],
+  controllers: [CartController],
 })
 export class CartModule {}
